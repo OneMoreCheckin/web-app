21ff66a5725d4325df70556176fdad51c57a2cf6
Manu <e.tabard@gmail.com>
2011-09-18 15:08:50 +0200

    sdfsdfsdf

--- js/stats.js
+++ js/stats.js
@@ -109,11 +109,13 @@ BadgeList = new (function () {
      $("#list_badges").append('<li class="box_badge" id="id_box_badge'+cnt+'"> <div class="img_badge"><img src="'+imgPath+'" /></div> <div class="infos_badges"> <p class="titre_badge"><a href="#">'+obj.name+'</a></p> <div class="cat_badge"><img src="'+obj.icon+'"/></div> <p class="description_badge">Do '+obj.more+' more check-in. '+obj.details+' </p> <!--<p class="findit_badge"><a href="">Find it now!</a> <span class="txt_hurry"></span></p> --></div><div class="pourcent_badge"><script>$(document).ready(function() {$("#progressbar'+cnt+'").progressbar({ value: '+(obj.complete == 0 ? 5 : obj.complete)+' });});</script> <div class="pastille"><p class="txt_pastille" style="">'+obj.complete+'%</p></div> <div id="progressbar'+cnt+'"></div></div></li>');
    }
    
-   $('#previous').click(function (){
+   $('#pagination #previous').click(function (){
+     BadgeList.reset();
      BadgeList.render('next');
    });
    
-   $('#next').click(function () {
+   $('#pagination #next').click(function () {
+     BadgeList.reset();
      BadgeList.render('prev');
    });
 });
